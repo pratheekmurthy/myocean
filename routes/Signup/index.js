fa93498cadc231558c7df92859d11ff6cf0d9443
goutam <gautamkumarerp@gmail.com>
2021-12-18 16:41:18 +0530

    latest

--- routes/Signup/index.js
+++ routes/Signup/index.js
@@ -6,21 +6,22 @@ const joiValidator = require('../../middleware/joi')
 const { fetchSignUpSchema } = require('../../schema/signup/index')
 const { fetchLocationsSchema } = require('../../schema/signup/index')
 const { fetchCustomerSchema } = require('../../schema/signup/index')
-const { fetchValidateEmail } = require('../../schema/signup/index')
-const { fetchValidateMobile } = require('../../schema/signup/index')
+const { fetchValidateEmailSchema } = require('../../schema/signup/index')
+const { fetchValidateMobileSchema } = require('../../schema/signup/index')
+const { saveUserDetailsSchema } = require('../../schema/signup/index')
 
 console.info("entered routes")
 router.get('/fetchnotifications', signUpController.fetchNotifications);
 router.get('/fetchalerts', signUpController.fetchAlerts); 
 router.get('/fetchsignup', joiValidator(fetchSignUpSchema, 'query'), signUpController.fetchSignUp);
-router.post('/saveuserdetails', signUpController.saveUserDetails);
+router.post('/saveuserdetails', joiValidator(saveUserDetailsSchema, 'body'), signUpController.saveUserDetails);
 router.get('/fetchcountries', signUpController.fetchCountries);
 router.get('/fetchlocations', joiValidator(fetchLocationsSchema, 'query'), signUpController.fetchLocations);
 router.get('/fetchmlocompanies', signUpController.fetchMloCompanies);
 router.get('/fetchCustomer', joiValidator(fetchCustomerSchema, 'query'), signUpController.fetchCustomer);
 router.get('/fetchdesignation', signUpController.fetchDesignation);
-router.get('/validateemail', joiValidator(fetchValidateEmail, 'query'), signUpController.validateEmail);
-router.get('/validatemobile', joiValidator(fetchValidateMobile, 'query'), signUpController.validateMobile);
+router.get('/validateemail', joiValidator(fetchValidateEmailSchema, 'query'), signUpController.validateEmail);
+router.get('/validatemobile', joiValidator(fetchValidateMobileSchema, 'query'), signUpController.validateMobile);
 router.post('/invitecolleague', signUpController.inviteColleague);
 
 module.exports = router;
