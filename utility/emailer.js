2f6868fab8798998e897d6b19cd8b6848f79a8c0
Nithin Muniswamy <nithinm50@gmail.com>
2021-12-18 16:43:42 +0530

    added nodemailer eith emailer template

---
+++ utility/emailer.js
@@ -0,0 +1,31 @@
+const nodemailer = require('nodemailer');
+const {emailSettings, emailerInfo} = require('./emailConfig')
+
+const handelEmails = (emailFrom, emailTo, emailTemplate, subject) => {
+    return new Promise((resolve, reject) => {
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport(emailSettings);
+    
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: emailFrom, // sender address
+            to: emailTo, // list of receivers
+            subject: subject, // Subject line
+            html: emailTemplate // html body
+        };
+    
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                reject(error)
+            }
+            
+            resolve({
+            "status": true,
+            "data": "Success" 
+            })
+        })
+    })
+}
+
+module.exports = handelEmails;
