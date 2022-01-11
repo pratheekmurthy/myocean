7eb3ddb721197ba57b5268f9755be7b492dfd926
goutam <gautamkumarerp@gmail.com>
2022-01-11 19:40:57 +0530

    terminal list

--- routes/terminal/index.js
+++ routes/terminal/index.js
@@ -3,10 +3,11 @@ const terminalController = require('../../controllers/terminal');
 
 const router = express.Router();
 const joiValidator = require('../../middleware/joi')
-const { terminalSchema } = require('../../schema/terminal/index')
+const { terminalSchema, QPORTTerminalDto } = require('../../schema/terminal/index')
 
 router.get('/GetQPORTCntry', terminalController.getQPORTCntry);
 router.get('/FetchQPORTLocList', terminalController.fetchQPORTLocList);
 router.get('/FetchQPORTTerList', joiValidator(terminalSchema), terminalController.fetchQPORTTerList);
+router.get('/FetchQPORTTerminal', joiValidator(QPORTTerminalDto), terminalController.fetchQPORTTerminal);
 
 module.exports = router;
